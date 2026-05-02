e5415f2ae7495fba1b31d8de4650a9fbe50b1a1d
Mouaz Brai <157323789+mouazbrai@users.noreply.github.com>
2026-05-02 20:52:33 +0100

    Update App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 // App
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Header from "../components/layout/Header";
 import MainLayout from "../components/layout/MainLayout";
 import "../styles/index.css";
